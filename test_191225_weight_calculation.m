dc300f0878f4e0d6b593753b64bec793c1e27b45
Hayoung Jung <niceterran36@postech.ac.kr>
2019-12-24 23:29:29 +0900

    a
    a

--- test_191225_weight_calculation.m
+++ test_191225_weight_calculation.m
@@ -1,75 +1,16 @@
-clc
-clear all;
+clc; clear all;
+addpath functions;
 addpath(genpath('external'));
-addpath 'functions';
+load('sg_mesh.mat');
+centers = sg_mesh.centers; v_segment = sg_mesh.assignment; V = sg_mesh.vertices; F = sg_mesh.faces;
+S = sg_mesh.bonestructure;
 
-%% load data
-mesh = load('tr_mesh.mat');
-V = transformed.vertices;
-F = transformed.faces;
+Sg1 = sg_mesh.segment.Sg1;Sg2 = sg_mesh.segment.Sg2;Sg3 = sg_mesh.segment.Sg3;Sg4 = sg_mesh.segment.Sg4;Sg5 = sg_mesh.segment.Sg5;Sg6 = sg_mesh.segment.Sg6;Sg7 = sg_mesh.segment.Sg7;Sg8 = sg_mesh.segment.Sg8;
+Sg9 = sg_mesh.segment.Sg9;Sg10 = sg_mesh.segment.Sg10;Sg11 = sg_mesh.segment.Sg11;Sg12 = sg_mesh.segment.Sg12;Sg13 = sg_mesh.segment.Sg13;Sg14 = sg_mesh.segment.Sg14;Sg15 = sg_mesh.segment.Sg15;
+Sg16 = sg_mesh.segment.Sg16; Sg17 = sg_mesh.segment.Sg17; Sg18 = sg_mesh.segment.Sg18; Sg19 = sg_mesh.segment.Sg19; Sg20 = sg_mesh.segment.Sg20; Sg21 = sg_mesh.segment.Sg21;
 
-% CoR Information
-centers = zeros(30,3);
-for i = 1:size(axes,2)
-centers(i,:) = (axes{1,i}(1:3,4))';
-end
-
-% Wrist = centers(2,:)
-% Digit1 CMC, IP, DIP = centers(3,4,5,:)
-% Digit2 MCP, PIP, DIP = centers(6,7,8,:)
-% Digit3 MCP, PIP, DIP = centers(9,10,11,:)
-% Digit4 MCP, PIP, DIP = centers(12,13,14,:)
-% Digit5 MCP, PIP, DIP = centers(16,17,18,:)
-
+%% Visualization mesh
 A = centers;
-figure() % point cloud 3D plotting
-hold on
-axis equal
-plot3(A(:,1),A(:,2),A(:,3),'b*') % CoR plotting
-% Link between CoRs
-plot3(A(1:2,1), A(1:2,2), A(1:2,3),'k-')
-plot3(A(2:5,1), A(2:5,2), A(2:5,3),'b-')
-plot3(A(6:8,1), A(6:8,2), A(6:8,3),'k-')
-plot3(A(9:11,1), A(9:11,2), A(9:11,3),'k-')
-plot3(A(12:14,1), A(12:14,2), A(12:14,3),'k-')
-plot3(A(16:18,1), A(16:18,2), A(16:18,3),'k-')
-plot3(A([2 6],1),A([2 6],2),A([2 6],3),'b-')
-plot3(A([2 9],1),A([2 9],2),A([2 9],3),'b-')
-plot3(A([2 12],1),A([2 12],2),A([2 12],3),'b-')
-plot3(A([2 16],1),A([2 16],2),A([2 16],3),'b-')
-scatter3(V(:,1),V(:,2),V(:,3),'.', 'MarkerEdgeColor',[217/255, 217/255, 217/255])
-hold off
-
-%% Segment visualization
-V2 = V;
-VLI = [];
-v_segment = transformed.assignments;
-for segment=1:18
-temLI = v_segment == segment;
-VLI = [VLI temLI];
-end 
-
-temLI = v_segment == 1; Sg1 = V2(temLI,:);
-temLI = v_segment == 2; Sg2 = V2(temLI,:);
-temLI = v_segment == 3; Sg3 = V2(temLI,:);
-temLI = v_segment == 4; Sg4 = V2(temLI,:);
-temLI = v_segment == 5; Sg5 = V2(temLI,:);
-temLI = v_segment == 6; Sg6 = V2(temLI,:);
-temLI = v_segment == 7; Sg7 = V2(temLI,:);
-temLI = v_segment == 8; Sg8 = V2(temLI,:);
-temLI = v_segment == 9; Sg9 = V2(temLI,:);
-temLI = v_segment == 10; Sg10 = V2(temLI,:);
-temLI = v_segment == 11; Sg11 = V2(temLI,:);
-temLI = v_segment == 12; Sg12 = V2(temLI,:);
-temLI = v_segment == 13; Sg13 = V2(temLI,:);
-temLI = v_segment == 14; Sg14 = V2(temLI,:);
-temLI = v_segment == 15; Sg15 = V2(temLI,:);
-temLI = v_segment == 16; Sg16 = V2(temLI,:);
-temLI = v_segment == 17; Sg17 = V2(temLI,:);
-temLI = v_segment == 18; Sg18 = V2(temLI,:);
-
-% segment color re-allocation
-
 figure()
 hold on
 axis equal
@@ -91,11 +32,27 @@ scatter3(Sg15(:,1),Sg15(:,2),Sg15(:,3),'.', 'MarkerEdgeColor',[191/255, 247/255,
 scatter3(Sg16(:,1),Sg16(:,2),Sg16(:,3),'.', 'MarkerEdgeColor',[247/255, 170/255, 20/255])
 scatter3(Sg17(:,1),Sg17(:,2),Sg17(:,3),'.', 'MarkerEdgeColor',[242/255, 62/255, 27/255])
 scatter3(Sg18(:,1),Sg18(:,2),Sg18(:,3),'.', 'MarkerEdgeColor',[191/255, 247/255, 20/255])
+scatter3(Sg19(:,1),Sg19(:,2),Sg19(:,3),'.', 'MarkerEdgeColor',[247/255, 170/255, 20/255])
+scatter3(Sg20(:,1),Sg20(:,2),Sg20(:,3),'.', 'MarkerEdgeColor',[242/255, 62/255, 27/255])
+scatter3(Sg21(:,1),Sg21(:,2),Sg21(:,3),'.', 'MarkerEdgeColor',[0, 0, 0])
+plot3(A(:,1),A(:,2),A(:,3),'b*')
+plot3(A(1:4,1), A(1:4,2), A(1:4,3),'k-')
+plot3(A(5:8,1), A(5:8,2), A(5:8,3),'k-')
+plot3(A(9:12,1), A(9:12,2), A(9:12,3),'k-')
+plot3(A(13:16,1), A(13:16,2), A(13:16,3),'k-')
+plot3(A(17:20,1), A(17:20,2), A(17:20,3),'k-')
+plot3(A([4 22],1),A([4 22],2),A([4 22],3),'b-')
+plot3(A([8 22],1),A([8 22],2),A([8 22],3),'b-')
+plot3(A([12 22],1),A([12 22],2),A([12 22],3),'b-')
+plot3(A([16 22],1),A([16 22],2),A([16 22],3),'b-')
+plot3(A([20 22],1),A([20 22],2),A([20 22],3),'b-')
+plot3(A([22 27],1),A([22 27],2),A([22 27],3),'k-')
+%scatter3(V(:,1),V(:,2),V(:,3),'.', 'MarkerEdgeColor',[217/255, 217/255, 217/255])
 hold off
 
 %% vertex id finder % drawer
 
-pick = [-5.0153 0.92919 0.38081]
+pick = [4.0035 -14.7856 42.0156]
 
 t = zeros(size(V,1),2); 
 for i = 1:size(V,1)
@@ -104,28 +61,12 @@ for i = 1:size(V,1)
 end 
 mindist_pt = min(t(:,2));
 [vertexIdx ,col] = find(t(:,2) == mindist_pt);
-main_segment = transformed.assignments(vertexIdx);
+main_segment = v_segment(vertexIdx);
 fprintf('VertexIdx = %.0f, Current segment = %.0f\n',vertexIdx, main_segment)
-
-% row and column information of target, wc_t = weight column for target
-wc_t = zeros(18,2); wc_t(:,1) = [1:18]';
-wc_t(:,2) = transformed.weights(vertexIdx,:)';
-LIX = wc_t(:,2) > 0;
-wc_t = wc_t(LIX,:);
-
-% weight print per joint
-if size(wc_t,1) == 2
-   fprintf('Joint%.0f = %3.2f, Joint%.0f = %3.2f\n',wc_t(1,1),wc_t(1,2),wc_t(2,1),wc_t(2,2))
-elseif size(wc_t,1) == 3
-   fprintf('Joint%.0f = %3.2f, Joint%.0f = %3.2f,Joint%.0f = %3.2f\n',wc_t(1,1),wc_t(1,2),wc_t(2,1),wc_t(2,2),wc_t(3,1),wc_t(3,2))
-elseif size(wc_t,1) == 4
-   fprintf('Joint%.0f = %3.2f, Joint%.0f = %3.2f,Joint%.0f = %3.2f, Joint%.0f = %3.2f\n',wc_t(1,1),wc_t(1,2),wc_t(2,1),wc_t(2,2),wc_t(3,1),wc_t(3,2),wc_t(4,1),wc_t(4,2))
-else
-   fprintf('Joint%.0f = %3.2f\n',wc_t(1,1),wc_t(1,2))
-end 
 
 if main_segment == 1
     SgV = Sg1;
+    jna = centers(22,:); 
 elseif main_segment == 2
     SgV = Sg2;
 elseif main_segment == 3
@@ -185,48 +126,14 @@ scatter3(A(wc_t(:,1),1),A(wc_t(:,1),2),A(wc_t(:,1),3), 'o', 'MarkerEdgeColor',[2
 scatter3(SgV(:,1),SgV(:,2),SgV(:,3),'.', 'MarkerEdgeColor',[242/255, 62/255, 27/255])
 hold off
 
-
-
-
 %%
+% function, initial parameter setting
+a = 1.3;
+c = 0.25;
+fx = a*exp(-((x-0.5)^2)/(2*c^2));
 
-for i = 1:3045
-    if FaceColor_mx(i,1) == 14
-       FaceColor_mx(i,2:4) = [242 62 27];
-    else
-       FaceColor_mx(i,2:4) = [0 0 0];
-    end 
-end 
+W = zeros(size(V,1),21);
 
-FaceColor_mx_n = FaceColor_mx(:,2:4);
+vertexIdx = 1;
 
-
-h1 = trisurf(F, V(:,1), V(:,2), V(:,3));
-    set(h1, 'FaceColor',[1 0.88 0.77]) %face color matrix 생성하여 개별값 삽입 가능
-    set(h1, 'EdgeColor', 'none');
-    set(h1, 'facealpha', 1);
-    
-    view(2);
-    axis equal;
-    light('Position', [3 5 7], 'Style', 'infinite');
-    lighting gouraud;
-    material dull;
-    
-    hold on;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% main influence joint = jna of assigned segment
