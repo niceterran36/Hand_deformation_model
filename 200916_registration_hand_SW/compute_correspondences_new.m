0a163c0d6cbf1fab6fd498a7d40a402279587d0a
niceterran36 <niceterran36@postech.ac.kr>
2020-09-24 01:38:57 +0900

    a
    a

--- 200916_registration_hand_SW/compute_correspondences_new.m
+++ 200916_registration_hand_SW/compute_correspondences_new.m
@@ -13,27 +13,47 @@ function pairs = compute_correspondences_new(mesh_vertices, mesh_normals, points
     distances = sqrt(sum(delta .^ 2, 2));
     % distances(distances>30) = 0
     LIX = distances < 30;
-    points_v_candidate = points.vertices(LIX,:)
-    points_v_candidate(:,4) = Idx_points(LIX)
-    points_n_candidates = points.normals(LIX,:)
+    points_v_candidate = points.vertices(LIX,:);
+    points_v_candidate(:,4) = Idx_points(LIX);
+    points_n_candidates = points.normals(LIX,:);
+    m = size(points_v_candidate,1);
     
+    A = zeros(m,m);
+    supply = ones(m,1);
+    demand = ones(1,m);
+    
+    normals = normalizerow(normals);
+    points_n_candidates = normalizerow(points_n_candidates);    
       
-    for i = 1 : m    
-        [~, j] = min(distances);
-        pairs(i, 1) = i;
-        pairs(i, 2) = j;
+    for i = 1 : n % for template_vertices
+
+       for j = 1 : m % for scan points
+
+            cos_angle = normals(i, :) * points_n_candidates(j, :)';
+            distance = norm(vertices(i, :) - points_v_candidate(j, 1:3));
+
+            if cos_angle < 0
+               cost = 1000;
+            else 
+               cost = distance;
+            end 
+
+            A(j,i) = cost;
+            
+       end 
+
     end
-
-
-
+    
+    Sol = Transportation_code(A, supply, demand);
+    
 end 
 
-figure()
-axis equal
-axis off
-hold on
-scatter3(vertices(:,1),vertices(:,2),vertices(:,3),'.', 'MarkerEdgeColor',[144/255, 228/255, 255/255]);
-scatter3(points_candidate(:,1),points_candidate(:,2),points_candidate(:,3),'.', 'MarkerEdgeColor',[220/255, 220/255, 220/255]);
-scatter3(centroid(1,1),centroid(1,2),centroid(1,3),'*','MarkerEdgeColor',[255/255, 0/255, 0/255]);
-view(-195,0);
-hold off
+% figure()
+% axis equal
+% axis off
+% hold on
+% view(-195,0);
+% scatter3(vertices(:,1),vertices(:,2),vertices(:,3),'.', 'MarkerEdgeColor',[144/255, 228/255, 255/255]);
+% scatter3(points_candidate(:,1),points_candidate(:,2),points_candidate(:,3),'.', 'MarkerEdgeColor',[220/255, 220/255, 220/255]);
+% scatter3(centroid(1,1),centroid(1,2),centroid(1,3),'*','MarkerEdgeColor',[255/255, 0/255, 0/255]);
+% hold off
