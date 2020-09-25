10a1ca455bb2962467ce73d9ae58fca06438d1d9
niceterran36 <niceterran36@postech.ac.kr>
2020-09-26 04:21:35 +0900

    r
    r

--- 200923_Vogel_by_Ivan/external/gptoolbox/wrappers/path_to_libigl.m
+++
@@ -1,8 +0,0 @@
-function s = path_to_libigl()
-  % PATH_TO_LIBIGL
-  %
-  % s = path_to_libigl()
-  %
-  % Returns path to libigl as string
-  s = '/usr/local/igl/libigl/';
-end
--- 200923_Vogel_by_Ivan/functions/function_rotation_matrix.m
+++
@@ -1,5 +0,0 @@
-function data = function_rotation_matrix(data, R)
-    data_copy = data;
-    data(:, 1) = R(1, 1)*data_copy(:, 1) + R(1, 2)*data_copy(:, 2) + R(1, 3)*data_copy(:, 3);
-    data(:, 2) = R(2, 1)*data_copy(:, 1) + R(2, 2)*data_copy(:, 2) + R(2, 3)*data_copy(:, 3);
-    data(:, 3) = R(3, 1)*data_copy(:, 1) + R(3, 2)*data_copy(:, 2) + R(3, 3)*data_copy(:, 3);
